6a45b574087bd028c7746ad53b0248d977da5574
JarateKing <scmacintosh@upei.ca>
2018-11-04 21:56:10 -0400

    made time tracker reset regularly

--- beowolf/Math/W_Time.cpp
+++ beowolf/Math/W_Time.cpp
@@ -3,10 +3,20 @@
 
 namespace wolf
 {
+	const double THRESHOLD = 5.0;
+
 	void Time::update()
 	{
 		lastTime = currentTime;
 		currentTime = glfwGetTime();
+
+		// prevent loss of precision
+		if (lastTime > THRESHOLD)
+		{
+			glfwSetTime(currentTime - THRESHOLD);
+			lastTime -= THRESHOLD;
+			currentTime -= THRESHOLD;
+		}
 	}
 
 	double Time::deltaTime()
