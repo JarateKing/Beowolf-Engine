531edfd94c4183937a45e2a8ac41c2faa6eefdab
JarateKing <scmacintosh@upei.ca>
2018-11-08 10:22:13 -0400

    added const corrected to projection matrix

--- beowolf/Math/W_ProjectionMatrix.cpp
+++ beowolf/Math/W_ProjectionMatrix.cpp
@@ -6,7 +6,7 @@ namespace wolf
 	const float FAR = 1000.0f;
 
 	// get aspect ratio from horizontal and vertical fov
-	float GetAspectRatio(glm::vec2 fov)
+	float GetAspectRatio(const glm::vec2& fov)
 	{
 		return glm::tan(DEG2RAD * fov.y / 2) * glm::tan(DEG2RAD * fov.x / 2);
 	}
@@ -26,7 +26,7 @@ namespace wolf
 	}
 
 	// get horizontal and vertical fov using a width and height
-	glm::vec2 GetFovCombo(float verticalFov, glm::vec2 screenSize)
+	glm::vec2 GetFovCombo(float verticalFov, const glm::vec2& screenSize)
 	{
 		float aspectRatio = ((float)screenSize.x) / fmax((float)screenSize.y, 1.0f);
 		return GetFovCombo(verticalFov, aspectRatio);
@@ -43,7 +43,7 @@ namespace wolf
 	{
 		return GetProjectionMatrix(GetFovCombo(verticalFov), false, 1.0f);
 	}
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov)
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov)
 	{
 		return GetProjectionMatrix(fov, false, 1.0f);
 	}
@@ -51,7 +51,7 @@ namespace wolf
 	// get projection matrix that is either perspective or orthographic
 	// perspective is defined by horizontal and vertical fov
 	// orthographic is defined by the distance where the view will be the same as perspective with the same fov values
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov, bool isOrtho, float orthoDistance)
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov, bool isOrtho, float orthoDistance)
 	{
 		float aspect = GetAspectRatio(fov);
 
@@ -68,7 +68,7 @@ namespace wolf
 	}
 
 	// get projection matrix that is some percent perspective, some percent orthographic
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov, float orthoPercent, float orthoDistance)
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov, float orthoPercent, float orthoDistance)
 	{
 		// special cases, percent breaks when <0 and >1
 		if (orthoPercent >= 1.0f)
--- beowolf/Math/W_ProjectionMatrix.h
+++ beowolf/Math/W_ProjectionMatrix.h
@@ -2,13 +2,13 @@
 
 namespace wolf
 {
-	float GetAspectRatio(glm::vec2 fov);
+	float GetAspectRatio(const glm::vec2& fov);
 	glm::vec2 GetScreenSize();
 	glm::vec2 GetFovCombo(float verticalFov, float aspectRatio);
-	glm::vec2 GetFovCombo(float verticalFov, glm::vec2 screenSize);
+	glm::vec2 GetFovCombo(float verticalFov, const glm::vec2& screenSize);
 	glm::vec2 GetFovCombo(float verticalFov);
 	glm::mat4 GetProjectionMatrix(float verticalFov);
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov);
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov, bool isOrtho, float orthoDistance);
-	glm::mat4 GetProjectionMatrix(glm::vec2 fov, float orthoPercent, float orthoDistance);
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov);
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov, bool isOrtho, float orthoDistance);
+	glm::mat4 GetProjectionMatrix(const glm::vec2& fov, float orthoPercent, float orthoDistance);
 }
