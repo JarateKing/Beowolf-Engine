b2132c15f609034f3685413582ac112df3fceedb
JarateKing <scmacintosh@upei.ca>
2018-11-08 10:17:40 -0400

    added const correctness to mousepos

--- beowolf/W_Input.h
+++ beowolf/W_Input.h
@@ -12,41 +12,41 @@ namespace wolf
 		MousePos(int x = 0, int y = 0) : x(x), y(y) {}
 
 		// get percent relative to thing
-		glm::vec2 relative(glm::vec2 size)
+		glm::vec2 relative(const glm::vec2& size) const
 		{
 			return glm::vec2(size.x / x, size.y / y);
 		}
-		glm::vec2 relative(double width, double height)
+		glm::vec2 relative(double width, double height) const
 		{
 			return glm::vec2(width / x, height / y);
 		}
-		glm::vec2 relative(glm::vec2 size, glm::vec2 offset)
+		glm::vec2 relative(const glm::vec2& size, const glm::vec2& offset) const
 		{
 			return glm::vec2(size.x / x - offset.x, size.y / y - offset.y);
 		}
-		glm::vec2 relative(double width, double height, double horizontalOffset, double verticalOffset)
+		glm::vec2 relative(double width, double height, double horizontalOffset, double verticalOffset) const
 		{
 			return glm::vec2(width / x - horizontalOffset, height / y - verticalOffset);
 		}
 
 		// operators
-		bool operator==(MousePos input) const
+		bool operator==(const MousePos& input) const
 		{
 			return (x == input.x) && (y == input.y);
 		}
-		MousePos operator+(MousePos input) const
+		MousePos operator+(const MousePos& input) const
 		{
 			return MousePos(x + input.x, y + input.y);
 		}
-		MousePos operator-(MousePos input) const
+		MousePos operator-(const MousePos& input) const
 		{
 			return MousePos(x - input.x, y - input.y);
 		}
-		MousePos operator*(MousePos input) const
+		MousePos operator*(const MousePos& input) const
 		{
 			return MousePos(x * input.x, y * input.y);
 		}
-		MousePos operator/(MousePos input) const
+		MousePos operator/(const MousePos& input) const
 		{
 			return MousePos(x / input.x, y / input.y);
 		}
