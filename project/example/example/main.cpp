8098654e2406d10ecb1e2bbaeaaa8d451515d33e
JarateKing <scmacintosh@upei.ca>
2019-05-05 12:35:23 -0300

    refactored main

--- project/example/example/main.cpp
+++ project/example/example/main.cpp
@@ -24,79 +24,82 @@ extern "C" FILE * __cdecl __iob_func(void)
 }
 //========================================================================
 
-int main( void )
+void setupGraphics(const char* windowTitle, int windowWidth, int windowHeight)
 {
-    int width, height, x;
-    double t;
-    
-    // Initialise GLFW
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
-    
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
-    glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-    // Open a window and create its OpenGL context
-    if( !glfwOpenWindow( 1280, 720, 0,0,0,0, 24,0, GLFW_WINDOW ) )
-    {
-        fprintf( stderr, "Failed to open GLFW window\n" );
-        
-        glfwTerminate();
-        exit( EXIT_FAILURE );
-    }
-    
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+	glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	if (!glfwOpenWindow(windowWidth, windowHeight, 0, 0, 0, 0, 24, 0, GLFW_WINDOW))
+	{
+		fprintf(stderr, "Failed to open GLFW window\n");
+
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
 #ifndef __APPLE__
 	glewExperimental = GL_TRUE;
 	glewInit();
 #endif
 
-    glfwSetWindowTitle( "Example" );
-    
-    // Ensure we can capture the escape key being pressed below
-    glfwEnable( GLFW_STICKY_KEYS );
-    
-    // Enable vertical sync (on cards that support it)
-    glfwSwapInterval( 1 );
+	glfwSetWindowTitle(windowTitle);
+
+	// Ensure we can capture the escape key being pressed below
+	glfwEnable(GLFW_STICKY_KEYS);
+
+	// vsync
+	glfwSwapInterval(1);
+}
+
+void updateGraphics()
+{
+	// set window bounds
+	int width, height;
+	glfwGetWindowSize(&width, &height);
+	height = height > 0 ? height : 1;
+	glViewport(0, 0, width, height);
+
+	// clear screen
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void updateGameLogic(Scene* scene)
+{
+	// update game
+	wolf::Time::Instance().update();
+	wolf::Input::Instance().update();
+	scene->Update();
+	scene->Render();
+}
+
+int main()
+{
+    setupGraphics("Example", 1280, 720);
     
 	BaseScene* scene = new BaseScene();
 	scene->Init();
 
-    do
-    {
-        glfwGetMousePos( &x, NULL );
-        
-        // Get window size (may be different than the requested size)
-        glfwGetWindowSize( &width, &height );
-        
-        // Special case: avoid division by zero below
-        height = height > 0 ? height : 1;
-        
-        glViewport( 0, 0, width, height );
-        
-        // Clear color buffer to gray
-        glClearColor( 0.5f, 0.5f, 0.5f, 0.0f );
-        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	while (glfwGetWindowParam(GLFW_OPENED))
+	{
+		updateGraphics();
+		updateGameLogic(scene);
 
-		wolf::Time::Instance().update();
-		wolf::Input::Instance().update();
-		scene->Update();
-		scene->Render();
-
-        // Swap buffers
-        glfwSwapBuffers();
-        
-    } // Check if the ESC key was pressed or the window was closed
-    while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
-          glfwGetWindowParam( GLFW_OPENED ) );
+		// swap buffers
+		glfwSwapBuffers();
+	}
     
-    // Close OpenGL window and terminate GLFW
     glfwTerminate();
-    
     exit( EXIT_SUCCESS );
 }
 
