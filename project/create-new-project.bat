549200f2baba2e0f6041fcbb53590ffd2917912a
JarateKing <scmacintosh@upei.ca>
2019-05-10 22:51:27 -0300

    added error checking for create project

--- project/create-new-project.bat
+++ project/create-new-project.bat
@@ -4,8 +4,21 @@
 if not [%1]==[] set projname=%1
 if [%1]==[] set /p projname="Name of new project: "
 
+if exist %projname% GOTO ERROR
+
+:: make copy of example folder
 move example example-temporary-to-rename
 mkdir example
 xcopy /E /EXCLUDE:ignored-files.txt example-temporary-to-rename example
 call rename-project.bat example %projname%
 move example-temporary-to-rename example
+
+:: we have successfully done what we need to
+GOTO DONE
+
+:ERROR
+echo ERROR
+pause
+GOTO DONE
+
+:DONE
