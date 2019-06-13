db7e172fbf969275cc03b213fae6b7aa912f339d
JarateKing <scmacintosh@upei.ca>
2019-06-12 21:44:30 -0300

    added localization table

---
+++ beowolf/text/W_TextTable.h
@@ -0,0 +1,25 @@
+#ifndef W_TEXTTABLE_H
+#define W_TEXTTABLE_H
+
+#include "W_Common.h"
+#include <string.h>
+#include <map>
+
+namespace wolf
+{
+	class TextTable
+	{
+	public:
+		TextTable();
+		~TextTable();
+		bool Load(const std::string& file);
+		bool SetLanguage(const std::string& lang);
+		std::string GetString(const std::string& id);
+
+	private:
+		std::string m_currentLanguage;
+		std::map<std::string, std::map<std::string, std::string>> m_lookupMap;
+	};
+}
+
+#endif
