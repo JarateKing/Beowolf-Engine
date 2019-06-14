da569ce9c7706713ef2b632325b85ab92d1e9ef9
JarateKing <scmacintosh@upei.ca>
2019-06-13 22:33:15 -0300

    fixed erroneous class carryover

--- beowolf/math/W_Math.h
+++ beowolf/math/W_Math.h
@@ -7,6 +7,6 @@ namespace wolf
 	public:
 		static bool isPrime(ull num);
 		static float lerp(float start, float end, float percent);
-		static double Math::lerp(double start, double end, float percent);
+		static double lerp(double start, double end, float percent);
 	};
 }
