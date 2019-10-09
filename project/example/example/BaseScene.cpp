19143cb12120610e5bbb8cd8bc3b1f58d81efd99
JarateKing <scmacintosh@upei.ca>
2019-10-09 16:04:21 -0300

    started reading binary file example

--- project/example/example/BaseScene.cpp
+++ project/example/example/BaseScene.cpp
@@ -2,13 +2,22 @@
 #define GLFW_NO_GLU
 #include "BaseScene.h"
 #include <iostream>
-#include "W_Math.h"
-#include "W_ProjectionMatrix.h"
-#include <iomanip>
-#include "sound/W_SoundEngine.h"
 
-const float DISTANCEFACTOR = 1.0f;
-wolf::SoundEngine SE;
+#include <fstream>
+#include <string>
+
+std::string readString(std::ifstream) {
+	return "";
+}
+
+unsigned int readInt(std::ifstream) {
+	return 0;
+}
+
+float readFloat(std::ifstream) {
+	return 0.0;
+}
+
 
 BaseScene::BaseScene()
 {
@@ -16,40 +25,10 @@ BaseScene::BaseScene()
 
 void BaseScene::Init()
 {
-	/////////////////////////
-	//Demo for Audio System//
-	/////////////////////////
-
-	//Listener and sound info
-	glm::vec3 listFor{ 0.0f, 0.0f, 1.0f };
-	glm::vec3 listUp{ 0.0f, 1.0f, 0.0f };
-	glm::vec3 listVel{ 10.0f, 0.0f, 0.0f };
-	glm::vec3 listPos{ -50.0f, 0.0f, 0.0f };
-	glm::vec3 soundPos{ 0.0f, 0.0f, 0.0f };
-	glm::vec3 soundVel{ 100.0f, 0.0f, 0.0f };
-
-	//Initialize System
-	SE.InitSystem();
-
-	//Creates loop sound, sets listener pos
-	SE.AddSound("media/drumloop.wav", "drumloop", false);
-	SE.PlayLoopSound("drumloop");
-	SE.Change3DSoundPos("drumloop", soundPos, soundVel);
-	SE.SetListenerAttr(listPos, listVel, listFor, listUp);
-
-	//Use this example for standard base sound
-	//SE.AddSound("media/drumloop.wav", "drumloop", true);
-	//SE.PlayBasicSound("drumloop");
 }
 
 void BaseScene::Update()
 {
-	//Updates the Position of listener of 3D sound, useless for stereo sound
-	FMOD_VECTOR pos = SE.GetListenerPos();
-	glm::vec3 listPos{ pos.x, pos.y, pos.z };
-	listPos.x = listPos.x + 0.1f;
-	SE.SetListenerAttr(listPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	SE.UpdateSystem();
 }
 
 void BaseScene::Render()
