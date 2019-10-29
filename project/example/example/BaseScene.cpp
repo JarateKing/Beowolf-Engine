1950fcabd0eace8f5e26115014f3bef7b3e4347b
JarateKing <scmacintosh@upei.ca>
2019-10-29 14:03:50 -0300

    made basescene show skeleton

--- project/example/example/BaseScene.cpp
+++ project/example/example/BaseScene.cpp
@@ -10,7 +10,6 @@
 
 wolf::DebugCamera* cam;
 wolf::BMWModel* test;
-wolf::BMWModel* teapot;
 
 BaseScene::BaseScene()
 {
@@ -22,9 +21,8 @@ void BaseScene::Init()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	cam = new wolf::DebugCamera(0, 0, glm::vec3(0, 0, -4));
-	test = new wolf::BMWModel("resources/models/tree.bmw", "resources/shaders/unlit_texture.vsh", "resources/shaders/unlit_texture.fsh");
-	teapot = new wolf::BMWModel("resources/models/teapot.bmw", "resources/shaders/cube.vsh", "resources/shaders/cube.fsh");
-	teapot->setTransform(glm::translate(glm::mat4(), glm::vec3(1.25, 0, 0)) * glm::scale(glm::vec3(0.25, 0.25, 0.25)));
+	test = new wolf::BMWModel("resources/models/myskeleton.bmw", "resources/shaders/cube.vsh", "resources/shaders/cube.fsh");
+	test->setTransform(glm::scale(glm::vec3(0.01, 0.01, 0.01)));
 }
 
 void BaseScene::Update()
@@ -32,9 +30,6 @@ void BaseScene::Update()
 	double delta = wolf::Time::Instance().deltaTime();
 	cam->Update(delta);
 	test->update(delta);
-	teapot->update(delta);
-
-	teapot->setTransform(teapot->getTransform() * glm::rotate(1.0f, glm::vec3(0, 1, 0)));
 }
 
 void BaseScene::Render()
@@ -43,13 +38,11 @@ void BaseScene::Render()
 	glDisable(GL_BLEND);
 
 	test->render(cam->GetViewMatrix(), glm::mat4(), false);
-	teapot->render(cam->GetViewMatrix(), glm::mat4(), false);
 
 	glDepthMask(false);
 	glEnable(GL_BLEND);
 
 	test->render(cam->GetViewMatrix(), glm::mat4(), true);
-	teapot->render(cam->GetViewMatrix(), glm::mat4(), true);
 }
 
 
