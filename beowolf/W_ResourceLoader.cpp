895b17b3cde0b4bb74923a2c0d0b59bbbf307d81
JarateKing <scmacintosh@upei.ca>
2019-11-15 23:07:47 -0400

    added warnings about fallbacks

--- beowolf/W_ResourceLoader.cpp
+++ beowolf/W_ResourceLoader.cpp
@@ -1,4 +1,5 @@
 #include "W_ResourceLoader.h"
+#include <iostream>
 #include <fstream>
 
 namespace wolf
@@ -7,24 +8,27 @@ namespace wolf
 		std::string filename = "resources/textures/" + name;
 		if (checkFileExists(filename))
 			return filename;
-		else
-			return "resources/textures/fallback.tga";
+		
+		std::cout << "Failed to find texture " << name << "\n";
+		return "resources/textures/fallback.tga";
 	}
 
 	std::string ResourceLoader::getVertexShader(std::string name) {
 		std::string filename = "resources/shaders/" + name;
 		if (checkFileExists(filename))
 			return filename;
-		else
-			return "resources/shaders/fallback.vsh";
+
+		std::cout << "Failed to find vertex shader " << name << "\n";
+		return "resources/shaders/fallback.vsh";
 	}
 
 	std::string ResourceLoader::getPixelShader(std::string name) {
 		std::string filename = "resources/shaders/" + name;
 		if (checkFileExists(filename))
 			return filename;
-		else
-			return "resources/shaders/fallback.fsh";
+
+		std::cout << "Failed to find pixel shader " << name << "\n";
+		return "resources/shaders/fallback.fsh";
 	}
 
 	std::string ResourceLoader::getModel(std::string name) {
