318f64b007258edaf3a17695d5258a9f7f6826c9
JarateKing <scmacintosh@upei.ca>
2019-12-07 13:30:35 -0400

    enforced specific sizes entirely

--- tools/font_converter/convert_font.bat
+++ tools/font_converter/convert_font.bat
@@ -1,5 +1,5 @@
 for /F "tokens=*" %%A in (fontsheet.txt) do (
-	msdfgen.exe -font "%1" %%A -o "text_%%A.png" -size 64 64 -autoframe
+	msdfgen.exe -font "%1" %%A -o "text_%%A.png" -size 64 64
 )
-montage -geometry 64x64+0+0 -tile x8 *.png -background black "%1.tga"
+montage -geometry 64x64+0+0 -tile 16x8 *.png -background black "%1.tga"
 del *.png
