07e445086beffc4f423f4ab7b2b2da5a8a62bdf9
JarateKing <scmacintosh@upei.ca>
2019-12-07 23:17:07 -0400

    added stub hiero_patch

---
+++ tools/font_converter/patch_hiero.py
@@ -0,0 +1,9 @@
+# hiero can provide lots of good information
+# like glyph width and various kerning stuff
+# but hiero's atlas is fairly tightly packed
+# while this converter doesn't pack them any
+# so we need to modify its x & y positioning
+
+tilesX = 16
+tilesY = 8
+tileSize = 64
