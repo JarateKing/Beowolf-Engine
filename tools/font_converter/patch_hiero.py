90a94889308d77a008e214fc9f2b39f2cdf6d06c
JarateKing <scmacintosh@upei.ca>
2019-12-07 23:22:35 -0400

    added base to patcher

--- tools/font_converter/patch_hiero.py
+++ tools/font_converter/patch_hiero.py
@@ -7,3 +7,14 @@
 tilesX = 16
 tilesY = 8
 tileSize = 64
+
+curPos = 0
+with open("fontsheet.txt") as charList:
+	for char in charList:
+		n = int(char)
+		print(n)
+		
+		# find the line in the file
+		# replace the line with new xpos / ypos based off curPos
+		
+		curPos = curPos + 1
