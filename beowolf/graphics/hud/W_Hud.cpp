c4dbf3965c33cff18232a08daaa15c3021920e57
JarateKing <scmacintosh@upei.ca>
2019-12-15 17:21:21 -0400

    added z order sorting

--- beowolf/graphics/hud/W_Hud.cpp
+++ beowolf/graphics/hud/W_Hud.cpp
@@ -2,8 +2,14 @@
 
 #include "W_TextBox.h"
 #include "W_HudImage.h"
+#include <algorithm>
 
 namespace wolf {
+
+	bool zSortCompare(HudElement* a, HudElement* b) {
+		return a->GetZ() >= b->GetZ();
+	}
+
 	Hud::Hud() {
 		m_localization = new TextTable();
 		m_localization->Load("resources/localization/engine.txt");
@@ -17,10 +23,12 @@ namespace wolf {
 		((TextBox*)m_elements[0])->SetTextAlignment(AL_Left);
 		((TextBox*)m_elements[0])->SetString("Engine_Name");
 		((TextBox*)m_elements[0])->SetTextColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+		m_elements[0]->SetZ(2.0f);
 
 		m_elements.push_back(new HudImage("particles/particle.dds"));
 		m_elements[1]->SetBounds(100, 100);
 		m_elements[1]->SetPos(20, 20);
+		m_elements[1]->SetZ(5.0f);
 	}
 
 	Hud::~Hud() {
@@ -28,7 +36,9 @@ namespace wolf {
 	}
 
 	void Hud::Update(float p_fDelta) {
-		((TextBox*)m_elements[0])->SetStringRaw("FPS: " + std::to_string(1.0f / p_fDelta));
+		//((TextBox*)m_elements[0])->SetStringRaw("FPS: " + std::to_string(1.0f / p_fDelta));
+
+		std::stable_sort(m_elements.begin(), m_elements.end(), zSortCompare);
 	}
 
 	void Hud::Render(glm::mat4 projection) {
