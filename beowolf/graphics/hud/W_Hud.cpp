c5fc36e6701ecab9cc2d03c50795bcedfce66d6b
JarateKing <scmacintosh@upei.ca>
2019-12-16 23:15:31 -0400

    removed hud data

--- beowolf/graphics/hud/W_Hud.cpp
+++ beowolf/graphics/hud/W_Hud.cpp
@@ -13,31 +13,9 @@ namespace wolf {
 
 	Hud::Hud() {
 		m_localization = new TextTable();
-		m_localization->Load("resources/localization/engine.txt");
 		m_localization->SetLanguage("ENGLISH");
 
-		m_fontlist["opensans.fnt"] = new Font("resources/fonts/", "opensans.fnt");
 
-		m_elements.push_back(new TextBox(m_fontlist["opensans.fnt"], m_localization));
-		m_elements[0]->SetBounds(1240, 220);
-		m_elements[0]->SetPos(20, 20);
-		((TextBox*)m_elements[0])->SetTextAlignment(AL_Left);
-		((TextBox*)m_elements[0])->SetString("Engine_FPS");
-		((TextBox*)m_elements[0])->SetTextColor(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-		m_elements[0]->SetZ(2.0f);
-
-		m_elements.push_back(new HudImage("particles/particle.dds"));
-		m_elements[1]->SetX(1920 - 80);
-		m_elements[1]->SetY(0);
-		m_elements[1]->SetW(80);
-		m_elements[1]->SetH(80);
-		m_elements[1]->SetZ(5.0f);
-
-		m_elements.push_back(new HudColorPanel(glm::vec4(0.0f, 0.0f, 0.0f, 0.3f)));
-		m_elements[2]->SetPos(0, 0);
-		m_elements[2]->SetW(1, true);
-		m_elements[2]->SetH(80);
-		m_elements[2]->SetZ(10.0f);
 	}
 
 	Hud::~Hud() {
