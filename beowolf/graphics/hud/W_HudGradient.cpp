036b6ce242a7ba37a26793fb13ba7f067eb9ef37
JarateKing <scmacintosh@upei.ca>
2019-12-18 12:35:05 -0400

    fixed conversions

--- beowolf/graphics/hud/W_HudGradient.cpp
+++ beowolf/graphics/hud/W_HudGradient.cpp
@@ -3,12 +3,12 @@
 
 namespace wolf {
 	HudGradient::HudGradient(glm::vec4 topleft, glm::vec4 topright, glm::vec4 bottomleft, glm::vec4 bottomright) {
-		wolf::Vertex square[] = { wolf::Vertex({ 0, 0, 1.0f, topleft.r, topleft.g, topleft.b, topleft.a, 0, 0}),
-								  wolf::Vertex({ 0, 1, 1.0f, bottomleft.r, bottomleft.g, bottomleft.b, bottomleft.a, 0, 1}),
-								  wolf::Vertex({ 1, 1, 1.0f, bottomright.r, bottomright.g, bottomright.b, bottomright.a, 1, 1}),
-								  wolf::Vertex({ 1, 1, 1.0f, bottomright.r, bottomright.g, bottomright.b, bottomright.a, 1, 1}),
-								  wolf::Vertex({ 1, 0, 1.0f, topright.r, topright.g, topright.b, topright.a, 1, 0}),
-								  wolf::Vertex({ 0, 0, 1.0f, topleft.r, topleft.g, topleft.b, topleft.a, 0, 0}) };
+		wolf::Vertex square[] = { wolf::Vertex({ 0, 0, 1.0f, (GLubyte)(topleft.r * 255), (GLubyte)(topleft.g * 255), (GLubyte)(topleft.b * 255), (GLubyte)(topleft.a * 255), 0, 0}),
+								  wolf::Vertex({ 0, 1, 1.0f, (GLubyte)(bottomleft.r * 255), (GLubyte)(bottomleft.g * 255), (GLubyte)(bottomleft.b * 255), (GLubyte)(bottomleft.a * 255), 0, 1}),
+								  wolf::Vertex({ 1, 1, 1.0f, (GLubyte)(bottomright.r * 255), (GLubyte)(bottomright.g * 255), (GLubyte)(bottomright.b * 255), (GLubyte)(bottomright.a * 255), 1, 1}),
+								  wolf::Vertex({ 1, 1, 1.0f, (GLubyte)(bottomright.r * 255), (GLubyte)(bottomright.g * 255), (GLubyte)(bottomright.b * 255), (GLubyte)(bottomright.a * 255), 1, 1}),
+								  wolf::Vertex({ 1, 0, 1.0f, (GLubyte)(topright.r * 255), (GLubyte)(topright.g * 255), (GLubyte)(topright.b * 255), (GLubyte)(topright.a * 255), 1, 0}),
+								  wolf::Vertex({ 0, 0, 1.0f, (GLubyte)(topleft.r * 255), (GLubyte)(topleft.g * 255), (GLubyte)(topleft.b * 255), (GLubyte)(topleft.a * 255), 0, 0}) };
 
 		g_pProgram = ProgramManager::CreateProgram(wolf::ResourceLoader::Instance().getShaders("hud_gradient"));
 		g_pVB = BufferManager::CreateVertexBuffer(&square, sizeof(Vertex) * 6);
