7fce41f07873d4ffcf06e0705fdcdebbca4daa4c
JarateKing <scmacintosh@upei.ca>
2020-01-04 00:06:23 -0400

    test lich model

--- project/example/example/BaseScene.cpp
+++ project/example/example/BaseScene.cpp
@@ -33,17 +33,15 @@ static glm::mat4 cull;
 static HexGrid* grid;
 wolf::MousePos mouse;
 static HexSelector* selector;
-wolf::BMWModel* test;
-wolf::BMWModel* test2;
 week2::ComponentHexPos hexPos;
 std::vector<int> testMove;
-//week9::AIPathfinder* pathFinder;
 wolf::Hud* testhud;
 glm::mat4 hudProjMat;
 
+wolf::BMWModel* test;
+
 BaseScene::BaseScene()
 {
-	//pathFinder->CreateInstance();
 }
 
 void BaseScene::Init()
@@ -51,18 +49,14 @@ void BaseScene::Init()
 	glEnable(GL_DEPTH_TEST);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	//auto shaders = wolf::ResourceLoader::Instance().getShaders("animatable_uv");
-	//test = new wolf::BMWModel(wolf::ResourceLoader::Instance().getModel("myskeleton.bmw"), shaders.first, shaders.second);
-	//test->setTransform(glm::translate(glm::vec3(0.0f, 20.0f, 20.0f)));
-
-	auto shaders2 = wolf::ResourceLoader::Instance().getShaders("unlit_texture");
-	test2 = new wolf::BMWModel(wolf::ResourceLoader::Instance().getModel("shield.bmw"), shaders2.first, shaders2.second);
-	//test2->setTransform(glm::scale(glm::vec3(0.01, 0.01, 0.01)));
+	auto shaders = wolf::ResourceLoader::Instance().getShaders("animatable_uv");
+	test = new wolf::BMWModel(wolf::ResourceLoader::Instance().getModel("lich/FreeLich.bmw"), shaders.first, shaders.second);
+	test->setTransform(glm::translate(glm::vec3(0, 0, 50)) * glm::scale(glm::vec3(5, 5, 5)));
 
 	cam = new Camera(0, 5.5, glm::vec3(0, 50.0f, 0));
 	cull = cam->GetViewMatrix();
 	wolf::SceneRenderer::getInstance().GenerateQuadtree(-10.0f, -10.0f, 20.0f, 20.0f);
-	grid = new HexGrid(15, 15, 5.0f, 1.0f, 20.0f, wolf::ResourceLoader::Instance().getTexture("tiles/Tile_Texs_1.tga"));
+	//grid = new HexGrid(15, 15, 5.0f, 1.0f, 20.0f, wolf::ResourceLoader::Instance().getTexture("tiles/Tile_Texs_1.tga"));
 	selector = new HexSelector(5.0f);
 	hexPos.SetGrid(grid);
 	testMove.push_back(1);
@@ -76,8 +70,6 @@ void BaseScene::Init()
 	testMove.push_back(1);
 	testMove.push_back(2);
 
-	//pathFinder->Instance()->Load("resources/objects/AIPathfindingDataTest.json");
-
 	testhud = new wolf::Hud("resources/hud/hud.json");
 	hudProjMat = glm::ortho(0.0f, 1920.0f, 1080.0f, 0.0f, 0.1f, 100.0f) * glm::lookAt(glm::vec3(0.0f, 0.0f, 4.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 }
@@ -88,46 +80,41 @@ void BaseScene::Update()
 	float delta = wolf::Time::Instance().deltaTime();
 	cam->Update(delta);
 
+	test->update(delta);
+
 	double fpsValue = round(wolf::Time::Instance().getFPS() * 10.0) / 10.0;
 	std::string fpsString = std::to_string(fpsValue);
 	testhud->SetVar("deltaMS", std::to_string(delta * 1000));
 	testhud->SetVar("fps", fpsString.substr(0, fpsString.find('.') + 2));
 	testhud->Update(delta);
 
-	//test->update(delta);
-	test2->update(delta);
-
-	int target = cam->CalculateIntersection(grid->GetHeights(), grid->GetPos(), 5.0f);
-	std::vector<float> heights = grid->GetHeights();
-	std::vector<glm::vec2> positions = grid->GetPos();
-	if (!(target < 0))
-	{
-		selector->Update(target, positions.at(target), heights.at(target));
-		grid->Update(target, delta);
-	}
-	wolf::SceneRenderer::getInstance().Update(delta, cam->GetViewMatrix());
-	if (wolf::Input::Instance().isKeyPressed(INPUT_KB_M))
-	{
-		hexPos.Move(testMove, 20.0f);
-		//test->setAnim("walk");
-	}
-
-	glm::vec3 old = hexPos.GetPos();
-	hexPos.Update(delta);
-	glm::vec3 dif = hexPos.GetPos() - old;
-	dif.y = 0;
-	float dir = 180.0f;
-	if (dif.x != 0 || dif.z != 0) {
-		dif = glm::normalize(dif);
-		dir = atan2(dif.x, dif.z) * RAD2DEG;
-		wasJustAnimated = true;
-	}
-	else if (wasJustAnimated) {
-		wasJustAnimated = false;
-		//test->setAnim("idle");
-	}
-
-	//test->setTransform(glm::translate(hexPos.GetPos()) * glm::rotate(dir, glm::vec3(0, 1.0f, 0)) * glm::scale(glm::vec3(0.025, 0.025, 0.025)));
+	//int target = cam->CalculateIntersection(grid->GetHeights(), grid->GetPos(), 5.0f);
+	//std::vector<float> heights = grid->GetHeights();
+	//std::vector<glm::vec2> positions = grid->GetPos();
+	//if (!(target < 0))
+	//{
+	//	selector->Update(target, positions.at(target), heights.at(target));
+	//	grid->Update(target, delta);
+	//}
+	//wolf::SceneRenderer::getInstance().Update(delta, cam->GetViewMatrix());
+	//if (wolf::Input::Instance().isKeyPressed(INPUT_KB_M))
+	//{
+	//	hexPos.Move(testMove, 20.0f);
+	//}
+	//
+	//glm::vec3 old = hexPos.GetPos();
+	//hexPos.Update(delta);
+	//glm::vec3 dif = hexPos.GetPos() - old;
+	//dif.y = 0;
+	//float dir = 180.0f;
+	//if (dif.x != 0 || dif.z != 0) {
+	//	dif = glm::normalize(dif);
+	//	dir = atan2(dif.x, dif.z) * RAD2DEG;
+	//	wasJustAnimated = true;
+	//}
+	//else if (wasJustAnimated) {
+	//	wasJustAnimated = false;
+	//}
 }
 
 void BaseScene::Render()
@@ -135,11 +122,11 @@ void BaseScene::Render()
 	glDepthMask(true);
 	glDisable(GL_BLEND);
 
-	wolf::SceneRenderer::getInstance().Render(cam->GetViewMatrix());
+	//wolf::SceneRenderer::getInstance().Render(cam->GetViewMatrix());
 	//grid->Render(cam->GetViewMatrix());
 	//selector->Render(cam->GetViewMatrix());
-	//test->render(cam->GetViewMatrix(), glm::mat4(), false);
-	test2->render(cam->GetViewMatrix(), glm::mat4(), false);
+	test->render(cam->GetViewMatrix(), glm::mat4(), false);
+	test->render(cam->GetViewMatrix(), glm::mat4(), true);
 
 	glDepthMask(false);
 	glEnable(GL_BLEND);
