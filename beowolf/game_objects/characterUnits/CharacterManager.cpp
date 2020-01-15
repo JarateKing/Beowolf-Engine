a4579d773b645149c4b41f30df8921809230c828
JarateKing <scmacintosh@upei.ca>
2020-01-15 00:37:17 -0400

    check for any items that a unit is touching

--- beowolf/game_objects/characterUnits/CharacterManager.cpp
+++ beowolf/game_objects/characterUnits/CharacterManager.cpp
@@ -45,6 +45,13 @@ void CharacterManager::Update(int p_target, float p_deltaT)
 	for (auto it = characters.begin(); it != characters.end(); it++)
 	{
 		it->Update(p_deltaT);
+
+		// check for items
+		for (int i = 0; i < items.size(); i++) {
+			if (glm::length(it->GetPos() - items[i].GetPos()) < 0.25) {
+				std::cout << 'y';
+			}
+		}
 	}
 
 	//Check if mouse pressed on top of hero
