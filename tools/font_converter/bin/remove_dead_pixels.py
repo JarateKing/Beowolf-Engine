c5cb408dfd47acd577ce6b3d39840d4f41de3a34
JarateKing <scmacintosh@upei.ca>
2020-01-16 12:32:57 -0400

    fixed up image format not always working right

--- tools/font_converter/bin/remove_dead_pixels.py
+++ tools/font_converter/bin/remove_dead_pixels.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 imgFile = Image.open(sys.argv[1])
+imgFile = imgFile.convert("RGB")
 pixels = imgFile.load()
 
 img = Image.new(imgFile.mode, imgFile.size)
@@ -10,7 +11,7 @@ newpixels = img.load()
 for i in range(img.size[0]):
 	for j in range(img.size[1]):
 		nonzeroCount = 0
-		for k in range(min(3, 0 if type(pixels[i, j]) is int else len(pixels[i, j]))):
+		for k in range(3):
 			if pixels[i, j][k] > 0:
 				nonzeroCount = nonzeroCount + 1
 		if nonzeroCount < 2:
