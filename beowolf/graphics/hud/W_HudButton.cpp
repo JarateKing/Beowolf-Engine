73449285295a6a32fa98ff9101106f94b38e0f43
JarateKing <scmacintosh@upei.ca>
2020-01-29 21:03:29 -0400

    made test click with proper bounds

--- beowolf/graphics/hud/W_HudButton.cpp
+++ beowolf/graphics/hud/W_HudButton.cpp
@@ -1,4 +1,8 @@
 #include "W_HudButton.h"
+#include "W_Input.h"
+#include "W_ProjectionMatrix.h"
+
+#include <iostream>
 
 namespace wolf {
 	HudButton::HudButton() {
@@ -10,7 +14,12 @@ namespace wolf {
 	}
 
 	void HudButton::Update(float p_fDelta) {
-
+		if (Input::Instance().isMouseReleased(INPUT_LMB)) {
+			float x = Input::Instance().getMousePos().x / ProjMatrix::GetScreenSize().x * 1920 - m_xPos;
+			float y = Input::Instance().getMousePos().y / ProjMatrix::GetScreenSize().y * 1080 - m_yPos;
+			if (x >= 0 && x <= m_xBound && y >= 0 && y <= m_yBound)
+				std::cout << "clicked\n";
+		}
 	}
 
 	void HudButton::Render(glm::mat4 proj) {
