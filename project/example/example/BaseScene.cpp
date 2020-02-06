ac7bfaa5a737ecaf10741bbd1c845415d698c120
JarateKing <scmacintosh@upei.ca>
2020-02-06 16:43:31 -0400

    removed debug ability to spawn items

--- project/example/example/BaseScene.cpp
+++ project/example/example/BaseScene.cpp
@@ -113,9 +113,6 @@ void BaseScene::Update()
 	cManager->Update(target, delta);
 	StateManager::getInstance().Update(delta);
 
-	if (wolf::Input::Instance().isKeyPressed(INPUT_KB_P))
-		cManager->SpawnItem(wolf::RNG::GetRandom(0, 200));
-
 	double fpsValue = round(wolf::Time::Instance().getFPS() * 10.0) / 10.0;
 	std::string fpsString = std::to_string(fpsValue);
 	testhud->SetVar("deltaMS", std::to_string(delta * 1000));
