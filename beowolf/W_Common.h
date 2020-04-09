329241ae20febbc3767b67e1c9f737c7699b5070
JarateKing <scmacintosh@upei.ca>
2020-04-09 20:40:39 -0300

    split vertex into own type file

--- beowolf/W_Common.h
+++ beowolf/W_Common.h
@@ -8,8 +8,8 @@
 #define COMMON_H
 
 #include "W_Types.h"
+#include "W_Vertex_Type.h"
 #include <string>
-#include "W_VertexDeclaration.h"
 
 #ifdef DEBUG
 #define GL_CHECK_ERROR() { GLenum e = glGetError(); if( e != GL_NO_ERROR ) { printf("GL ERROR: %x\n", e); } }
@@ -19,115 +19,6 @@
 
 namespace wolf
 {
-	struct Vertex
-	{
-		GLfloat x, y, z;
-		GLubyte r, g, b, a;
-		GLfloat u, v;
-		GLfloat normalDirX = 0.0f;
-		GLfloat normalDirY = 1.0f;
-		GLfloat normalDirZ = 0.0f;
-		glm::vec4 boneIndices;
-		glm::vec4 boneWeights;
-
-		void setNormalDir(glm::vec3 input)
-		{
-			normalDirX = input.x;
-			normalDirY = input.y;
-			normalDirZ = input.z;
-		}
-
-		void setColor(glm::vec3 color)
-		{
-			r = color.r * 255;
-			g = color.g * 255;
-			b = color.b * 255;
-		}
-
-		void setColor(glm::vec4 color)
-		{
-			r = color.r * 255;
-			g = color.g * 255;
-			b = color.b * 255;
-			a = color.a * 255;
-		}
-
-		glm::vec3 xyz()
-		{
-			return glm::vec3(x, y, z);
-		}
-
-		glm::vec2 xy()
-		{
-			return glm::vec2(x, y);
-		}
-
-		glm::vec2 xz()
-		{
-			return glm::vec2(x, z);
-		}
-
-		glm::vec2 yz()
-		{
-			return glm::vec2(y, z);
-		}
-
-		Vertex operator +(const Vertex other)
-		{
-			return {
-				x + other.x,
-				y + other.y,
-				z + other.z,
-				(GLubyte)(r + other.r),
-				(GLubyte)(g + other.g),
-				(GLubyte)(b + other.b),
-				(GLubyte)(a + other.a),
-				u + other.u,
-				v + other.v
-			};
-		}
-
-		Vertex operator -(const Vertex other)
-		{
-			return {
-				x - other.x,
-				y - other.y,
-				z - other.z,
-				(GLubyte)(r - other.r),
-				(GLubyte)(g - other.g),
-				(GLubyte)(b - other.b),
-				(GLubyte)(a - other.a),
-				u - other.u,
-				v - other.v
-			};
-		}
-
-		Vertex operator *(const Vertex other)
-		{
-			return {
-				x * other.x,
-				y * other.y,
-				z * other.z,
-				(GLubyte)(r * other.r),
-				(GLubyte)(g * other.g),
-				(GLubyte)(b * other.b),
-				(GLubyte)(a * other.a),
-				u * other.u,
-				v * other.v
-			};
-		}
-
-		static void applyAttributes(VertexDeclaration* decl)
-		{
-			decl->AppendAttribute(AT_Position, 3, CT_Float);
-			decl->AppendAttribute(AT_Color, 4, CT_UByte);
-			decl->AppendAttribute(AT_TexCoord1, 2, CT_Float);
-			decl->AppendAttribute(AT_Normal, 3, CT_Float);
-			decl->AppendAttribute(AT_BoneIndices, 4, CT_Float);
-			decl->AppendAttribute(AT_BoneWeight, 4, CT_Float);
-		}
-	};
-
 	// Loads in a whole file and returns the contents. User is responsible for then
 	// delete[]-ing the data. Returns 0 if file not able to be loaded.
 	void* LoadWholeFile(const std::string& p_strFile);
