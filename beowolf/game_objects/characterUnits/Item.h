d55cba73f9396102203a7571eef022367ca98105
JarateKing <scmacintosh@upei.ca>
2020-04-14 10:41:24 -0300

    commented + const correctness item

--- beowolf/game_objects/characterUnits/Item.h
+++ beowolf/game_objects/characterUnits/Item.h
@@ -10,23 +10,23 @@
 class Item
 {
 	public:
-		Item(std::string bmwFile, std::string shaderFile, int startTile, std::string name, HexGrid* grid);
+		Item(const std::string& bmwFile, const std::string& shaderFile, const int& startTile, const std::string& name, HexGrid* grid);
 		~Item();
-		void Render(glm::mat4 view, glm::mat4 proj, glm::mat4 lightSpaceMatrix, wolf::RenderFilterType type, bool shadowPass, unsigned int depthMapTexture);
-		void Update(float deltaT);
-		void SetLighting(glm::vec4 ambLight, glm::vec4 difLight, glm::vec3 lightDir);
-		void SetLightingDir(glm::vec3);
+		void Render(const glm::mat4& view, const glm::mat4& proj, const glm::mat4& lightSpaceMatrix, const wolf::RenderFilterType& type, const bool& shadowPass, const unsigned int& depthMapTexture);
+		void Update(const float& deltaT);
+		void SetLighting(const glm::vec4& ambLight, const glm::vec4& difLight, const glm::vec3& lightDir);
+		void SetLightingDir(const glm::vec3&);
 		std::string GetName();
 		int GetTile();
 		glm::vec3 GetPos();
 
 	private:
-		std::vector<std::string> soundFiles;
-		wolf::BMWModel* model;
-		int currTile;
-		std::string name;
-		week2::ComponentHexPos pos;
-		float scale = 0.25f;
+		std::vector<std::string> m_soundFiles;
+		wolf::BMWModel* m_model;
+		int m_currTile;
+		std::string m_name;
+		week2::ComponentHexPos m_hexpos;
+		float m_scale = 0.25f;
 
 		float m_bobTime;
 		glm::vec3 m_pos;
